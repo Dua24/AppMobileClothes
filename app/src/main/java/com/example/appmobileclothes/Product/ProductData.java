f4cda4a46c7753997e9464e5b88aba4dbeddf1b8
Lồ Sìn Dậu <89368649+dau12345@users.noreply.github.com>
2023-03-27 10:45:35 +0700

    BottomNaviMenu

--- app/src/main/java/com/example/appmobileclothes/Product/ProductData.java
+++ app/src/main/java/com/example/appmobileclothes/Product/ProductData.java
@@ -1,16 +1,16 @@
-package com.example.appmobileclothes.Product;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import com.example.appmobileclothes.R;
-
-public class ProductData extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_detail);
-    }
-}
+//package com.example.appmobileclothes.Product;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//import com.example.appmobileclothes.R;
+//
+//public class ProductData extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_product_detail);
+//    }
+//}
