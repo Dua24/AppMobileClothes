9a5665badb3dbcb54a66832f19870fc4f9e92b77
thiennguyen2310 <nhthien2310@gmail.com>
2023-03-29 03:05:56 +0700

    update giao diện chi tiết sản phẩm

--- app/src/main/java/com/example/appmobileclothes/Product/ProductData.java
+++ app/src/main/java/com/example/appmobileclothes/Product/ProductData.java
@@ -1,16 +1,4 @@
-//package com.example.appmobileclothes.Product;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//
-//import com.example.appmobileclothes.R;
-//
-//public class ProductData extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_product_detail);
-//    }
-//}
+package com.example.appmobileclothes.Product;
+
+public class ProductData {
+}
