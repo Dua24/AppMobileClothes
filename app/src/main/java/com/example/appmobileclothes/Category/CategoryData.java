c106eb5f740a93d043a0e6f9975904a5773aabe9
Lồ Sìn Dậu <89368649+dau12345@users.noreply.github.com>
2023-03-30 21:31:00 +0700

    Update category recyclerview, add searchview

--- app/src/main/java/com/example/appmobileclothes/Category/CategoryData.java
+++ app/src/main/java/com/example/appmobileclothes/Category/CategoryData.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class CategoryData {
     public static ArrayList<Category> generatePhotoData() {
         ArrayList<Category> categories = new ArrayList<Category>();
-        categories.add(new Category(0, "https://cdn-icons-png.flaticon.com/512/4488/4488007.png", "Tops"));
-        categories.add(new Category(1, "https://cdn-icons-png.flaticon.com/512/4488/4488090.png", "Jackets"));
-        categories.add(new Category(2, "https://cdn-icons-png.flaticon.com/512/4488/4488032.png", "Bottoms"));
-        categories.add(new Category(3, "https://cdn-icons-png.flaticon.com/512/4488/4488070.png", "Shoes"));
-        categories.add(new Category(4, "https://cdn-icons-png.flaticon.com/512/4488/4488094.png", "Accessories"));
+        categories.add(new Category(0, "https://cdn-icons-png.flaticon.com/512/4488/4488007.png", "T-shirts"));
+        categories.add(new Category(1, "https://cdn-icons-png.flaticon.com/512/4488/4488016.png", "Shirts"));
+        categories.add(new Category(2, "https://cdn-icons-png.flaticon.com/512/4488/4488014.png", "Outwears"));
+        categories.add(new Category(3, "https://cdn-icons-png.flaticon.com/512/4488/4488032.png", "Pants"));
+        categories.add(new Category(4, "https://cdn-icons-png.flaticon.com/512/4488/4488027.png", "Shorts"));
+        categories.add(new Category(5, "https://cdn-icons-png.flaticon.com/512/4488/4488070.png", "Shoes"));
+        categories.add(new Category(6, "https://cdn-icons-png.flaticon.com/512/4488/4488094.png", "Accessories"));
 
         return categories;
     }
